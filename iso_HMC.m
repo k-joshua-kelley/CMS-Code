4f69d7cbd8febb8c1f4df1226695f5435e3e469c
Joshua Kelley <k.ke11ey.j0sh@gmail.com>
2026-05-22 10:41:03 -0600

    added iso_HMC

---
+++ iso_HMC.m
@@ -0,0 +1,38 @@
+clear; clc; close all;
+
+rng("shuffle");
+
+data = load("Data/iso_data_stats.mat");
+T    = data.T;
+f    = data.f;
+Nx   = 2^7;
+dr   = data.dr;
+obs  = data.mu;
+kD   = data.kappa;
+lnsx = log(2);
+lnsy = log(2);
+
+priors = load("iso_priors.mat");
+data = load("iso_MAP_results.mat");
+
+hmc_params = load("Tuned_Smp_Params.mat");
+
+smp = hmcSampler( ...
+    @(x) log_post_obj(x, kD, obs, lnsx, lnsy, f, Nx, dr, T, priors.priors), ...
+    data.x, CheckGradient=false, StepSize=0.025, NumSteps=10, ...
+    MassVector=hmc_params.massVector ...
+);
+
+[chain,endpoint,accratio] = drawSamples(smp, Burnin=100, NumSamples=100);
+save("samples_Nsteps10_Burn100.mat", "chain", "endpoint", "accratio");
+
+function [nlp, grad] = log_post_obj(x, kD, obs, lnsx, lnsy, f, Nx, dr, T, priors_new)
+    disp(compose(strjoin(repmat("%6.2f", length(x), 1), " "), x(:).'))
+    if nargout < 2
+        nlp = iso_nl_post(x(1:7), x(8:14), x(15), x(16), x(17:23), x(24:30), x(31), x(32), x(33), x(34:37), kD, obs, lnsx, lnsy, f, Nx, dr, T, priors_new);
+    else
+        [nlp, grad] = iso_nl_post(x(1:7), x(8:14), x(15), x(16), x(17:23), x(24:30), x(31), x(32), x(33), x(34:37), kD, obs, lnsx, lnsy, f, Nx, dr, T, priors_new);
+        grad = -grad;
+    end
+    nlp = -nlp;
+end
