dcc1937b00b5144fe3535556d4d0cfdae80018dc
Joshua Kelley <k.ke11ey.j0sh@gmail.com>
2026-05-23 21:33:21 -0600

    fixed iso_HMC save file name

--- iso_HMC.m
+++ iso_HMC.m
@@ -2,6 +2,9 @@ clear; clc; close all;
 
 rng("shuffle");
 
+ts = string(datetime("now", 'Format', 'yyyy-MM-dd_HH-mm-ss'));
+save("samples_Nsteps10_Burn100_Thin2_"+ts+".mat", "ts");
+
 data = load("Data/iso_data_stats.mat");
 T    = data.T;
 f    = data.f;
@@ -23,11 +26,10 @@ smp = hmcSampler( ...
     MassVector=hmc_params.massVector ...
 );
 
-[chain,endpoint,accratio] = drawSamples(smp, Burnin=100, NumSamples=100);
-save("samples_Nsteps10_Burn100.mat", "chain", "endpoint", "accratio");
+[chain,endpoint,accratio] = drawSamples(smp, Burnin=100, NumSamples=500, ThinSize=2, VerbosityLevel=1);
+save("samples_Nsteps10_Burn100_Thin2_"+ts+".mat", "chain", "endpoint", "accratio");
 
 function [nlp, grad] = log_post_obj(x, kD, obs, lnsx, lnsy, f, Nx, dr, T, priors_new)
-    disp(compose(strjoin(repmat("%6.2f", length(x), 1), " "), x(:).'))
     if nargout < 2
         nlp = iso_nl_post(x(1:7), x(8:14), x(15), x(16), x(17:23), x(24:30), x(31), x(32), x(33), x(34:37), kD, obs, lnsx, lnsy, f, Nx, dr, T, priors_new);
     else
